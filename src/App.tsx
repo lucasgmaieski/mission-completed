581580d000c548fd0ac7d5f819d19e4c56f7bcfc
Lucas Maieski <lucasgrigol@gmail.com>
2023-04-08 18:54:05 -0300

    instalado react-icons e json-server

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { Header} from './components/Header';
 import './App.css';
 import { CreateTask } from './components/CreateTask';
-import { Main } from './components/Main';
-import { TodoList } from './components/TodoList';
+import { Container } from './components/Container';
+// import { TodoList } from './components/TodoList';
 
 const App = () => {
     return (
-        <Main bgMain="../src/assets/bg.jpg" >
+        <Container bgMain="../src/assets/bg.jpg" >
             <div className='max-w-3xl m-auto bg-black backdrop-blur-sm bg-opacity-70 border-2 rounded border-slate-800 shadow shadow-cyan-700 px-20'>
                 <Header />
                 <CreateTask />
                 {/* <TodoList/> */}
             </div>
-        </Main>
+        </Container>
         
     );
 }
