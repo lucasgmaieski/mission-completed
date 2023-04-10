ced1f0fec96a1bf8927c168be727d4d4e7268b79
Lucas Maieski <lucasgrigol@gmail.com>
2023-04-09 22:47:30 -0300

    criado função para classificar pelo status

--- src/components/CreateTask.tsx
+++ src/components/CreateTask.tsx
@@ -1,19 +1,25 @@
 import { useEffect, useState } from 'react';
-import { Task } from './Task';
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
 
-const API = "http://localhost:5000";
+export const API = "http://localhost:5000";
 export type Task = {
     idTask: number;
+    id: number;
     titleTask: string;
     durationTask: number;
     completedTask:boolean;
 }
+
+
 export const CreateTask = () => {
 
     const [title, setTitle] = useState<string>("");
     const [duration, setDuration] = useState<number>(0);
-    const [loading, setLoading] = useState<boolean>(false)
+    const [loading, setLoading] = useState<boolean>(false);
     const [list, setList] = useState<Task[]>([]);
+    const [activeButton, setActiveButton] = useState(1);
+    const [completed, setCompleted] = useState<boolean>(false);
+
 
     //Load todos on page load
     useEffect(() => {
@@ -30,13 +36,34 @@ export const CreateTask = () => {
         }
         loadData();
     }, []);
+    const handleCheckTask = async (item: Task) => {
+        console.log(item);
 
+        item.completedTask = !item.completedTask;
+        const data = await fetch(API + "/todos/" + item.id, {
+            method: "PUT",
+            body: JSON.stringify(item),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then((res) => res.json())
+        .then((data) => data)
+        
+        console.log(data);
+        setList((prevState) => prevState.map((t) => (t.id === data.id ? (t = data) : t)));
+        
+    }
+    const toggleActiveBtn = (id: number) => {
+        setActiveButton(id);
+    }
     const handleCreateTask = async () => {
         const newTask: Task = {
             idTask: Math.random(),
             titleTask: title,
             durationTask: duration,
             completedTask: false,
+            id: Math.random(),
         }
 
         await fetch(API + "/todos", {
@@ -50,10 +77,15 @@ export const CreateTask = () => {
         setTitle("");
         setDuration(0);
     }
-
-    if(loading) {
-        return <p>Carregando...</p>
+    
+    const handleDelete = async (id: number) => {
+        await fetch(API + "/todos/" + id, {
+            method: "DELETE"
+        });
+        setList((prevState) => prevState.filter((item) => item.id !== id));
+        console.log(id);
     }
+
     console.log("lista que não atualiza na hora");
     console.log(list);
     return (
@@ -81,16 +113,35 @@ export const CreateTask = () => {
 
             <h2 className='text-center mb-2 text-3xl'>Lista de tarefas:</h2>
             <div className='mb-4'>
-                <button className='rounded-none border-x-0 border-t-0 border-b-2 border-cyan-900 w-1/3 bg-black  hover:bg-white hover:text-black focus:outline-none active:border-white'>Todas</button>
-                <button className='rounded-none border-x-0 border-t-0 border-b-2 border-cyan-900 w-1/3 bg-black  hover:bg-white hover:text-black focus:outline-none active:border-white'>Ativas</button>
-                <button className='rounded-none border-x-0 border-t-0 border-b-2 border-cyan-900 w-1/3 bg-black  hover:bg-white hover:text-black focus:outline-none active:border-white'>Concluidas</button>
+                <button className={`rounded-none border-x-0 border-t-0 border-b-4 border-cyan-900 w-1/3 bg-black  hover:bg-white hover:text-black focus:outline-none active:border-white ${activeButton === 1 ? 'border-white' : '' }`} onClick={() => toggleActiveBtn(1)}>Todas</button>
+                <button className={`rounded-none border-x-0 border-t-0 border-b-4 border-cyan-900 w-1/3 bg-black  hover:bg-white hover:text-black focus:outline-none active:border-white ${activeButton === 2 ? 'border-white' : '' }`} onClick={() => toggleActiveBtn(2)}>Ativas</button>
+                <button className={`rounded-none border-x-0 border-t-0 border-b-4 border-cyan-900 w-1/3 bg-black  hover:bg-white hover:text-black focus:outline-none active:border-white ${activeButton === 3 ? 'border-white' : '' }`} onClick={() => toggleActiveBtn(3)}>Concluidas</button>
             </div>
+
+        
             {list.length === 0 && <p>Não há tarefas!</p>}
-            {list.map((item, key) => (
-                <Task key={key} idTask={item.idTask} titleTask={item.titleTask} durationTask={item.durationTask} completedTask={item.completedTask}/>
+            {activeButton === 1 && list.map((item, key) => (
+                <div>
+                    <h3 className={item.completedTask ? "text-orange-500 line-through" : ""}>{item.titleTask}</h3>
+                    <p className="italic">Duração: {item.durationTask}h</p>
+                    <div className="flex"> <i className='w-fit' onClick={() => handleCheckTask(item)}> {item.completedTask ? <BsBookmarkCheckFill /> : <BsBookmarkCheck />} </i> <i className='w-fit'><BsTrash onClick={() => (handleDelete(item.id))} /></i></div>
+                </div>
+            )) }
+            {activeButton === 2 && list.filter((item) => item.completedTask === false).map((item, key) => (
+                <div>
+                    <h3 className={item.completedTask ? "text-orange-500 line-through" : ""}>{item.titleTask}</h3>
+                    <p className="italic">Duração: {item.durationTask}h</p>
+                    <div className="flex"> <i className='w-fit' onClick={() => handleCheckTask(item)}> {item.completedTask ? <BsBookmarkCheckFill /> : <BsBookmarkCheck />} </i> <i className='w-fit'><BsTrash onClick={() => (handleDelete(item.id))} /></i></div>
+                </div>
+            ))}
+            {activeButton === 3 && list.filter((item) => item.completedTask === true).map((item, key) => (
+                <div>
+                    <h3 className={item.completedTask ? "text-orange-500 line-through" : ""}>{item.titleTask}</h3>
+                    <p className="italic">Duração: {item.durationTask}h</p>
+                    <div className="flex"> <i className='w-fit' onClick={() => handleCheckTask(item)}> {item.completedTask ? <BsBookmarkCheckFill /> : <BsBookmarkCheck />} </i> <i className='w-fit'><BsTrash onClick={() => (handleDelete(item.id))} /></i></div>
+                </div>
             ))}
             
         </>
-        
     );
 }
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,24 +1,38 @@
 import { useState } from "react";
 import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
+import { API, CreateTask } from "./CreateTask";
 
 type Props = {
     idTask: number;
+    id: number;
 
     titleTask: string;
     durationTask: number;
     completedTask: boolean;
+    listTask: Props[]
 }
 
-export const Task = ({idTask, titleTask, durationTask, completedTask}: Props) => {
+export const Task = ({listTask, id, idTask, titleTask, durationTask, completedTask}: Props) => {
     const [completed, setCompleted] = useState<boolean>(completedTask);
+    const [list, setList] = useState<Props[]>(listTask);
+
     const handleCheckTask = () => {
+
         {completed ? setCompleted(false) : setCompleted(true) }
+    }
+    const handleDelete = async (id: number) => {
+        await fetch(API + "/todos/" + id, {
+            method: "DELETE"
+        });
+        setList((prevState) => prevState.filter((item) => item.id !== id));
+        <CreateTask />
+        console.log(id);
     }
     return (
         <>
             <h3 className={completed ? "text-orange-500 line-through" : ""}>{titleTask}</h3>
-            <p>Duração: {durationTask}h</p>
-            <div> <i onClick={handleCheckTask}> {completed ? <BsBookmarkCheckFill /> : <BsBookmarkCheck />} </i> <i><BsTrash /></i></div>
+            <p className="italic">Duração: {durationTask}h</p>
+            <div className="flex"> <i className='w-fit' onClick={handleCheckTask}> {completed ? <BsBookmarkCheckFill /> : <BsBookmarkCheck />} </i> <i className='w-fit'><BsTrash onClick={() => (handleDelete(id))} /></i></div>
         </>
     );
 }
